0889803f1079d84de9d7cad945fd9a06dbec7c97
sealandsigh <zhujiajunzhu@qq.com>
2020-01-03 23:26:23 +0800

    列表常用方法

---
+++ onestage/步骤1/项目中使用列表.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2020/1/3
+
+str = '张三,30,2000'
+l = str.split(',')
+print(l)
+emp_list = []
+
+while True:
+    info = input('输入员工信息:' )
+    if info == "":
+        break
+    info_list = info.split(',')
+    print(info_list)
+    emp_list.append(info_list)
+
+print(emp_list)
