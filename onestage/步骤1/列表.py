4916c4424fd0412256e9ed0b467ce2215d762acd
sealandsigh <zhujiajunzhu@qq.com>
2020-01-03 18:18:17 +0800

    列表

---
+++ onestage/步骤1/列表.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2020/1/3
+
+i = 0
+count = 0
+a = [1,2]
+print(a)
+
+print(len(a))
+
+for p in a:
+    if p == 1:
+        print('ok',i)
+    i += 1
