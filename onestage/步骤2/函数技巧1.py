9fea60595edd6b0f185a6273faace27d178baf13
sealandsigh <zhujiajunzhu@qq.com>
2020-01-07 23:27:45 +0800

    函数的使用技巧

---
+++ onestage/步骤2/函数技巧1.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2020/1/7
+
+# 序列传参
+def calc(a,b,c):
+    return (a+b) *c
+
+l = [1,5,10]
+print(calc(*l))
+
+# 字典传参
+
+def health_check(name,age,*,height,weight,hr,hbp,lbp,glu):
+    print("您的身体健康")
+
+param = {"name":'zhujiajun',"age":23,"height":23,"weight":23,"hr":23,"hbp":23,"lbp":23,"glu":23}
+health_check(**param)
