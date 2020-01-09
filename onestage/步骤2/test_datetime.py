c07d27d4b4e514e7c0c565cbd00839053d9190be
sealandsigh <zhujiajunzhu@qq.com>
2020-01-09 22:54:27 +0800

    test_datetime

---
+++ onestage/步骤2/test_datetime.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2020/1/9
+
+import datetime
+
+now_time = datetime.datetime.now()
+
+print("now:{0}".format(datetime.datetime.now()))
---
+++ onestage/步骤2/模块和包.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2020/1/9
+
+"""
+这是一个注释
+"""
+
+import sys
+
+print(sys.path)
