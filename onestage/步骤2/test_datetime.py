1c097c3bd844f0655d51bc841fe929b1057bfa01
sealandsigh <zhujiajunzhu@qq.com>
2020-01-10 22:14:57 +0800

    test_datetime

--- onestage/步骤2/test_datetime.py
+++ onestage/步骤2/test_datetime.py
@@ -7,3 +7,13 @@ import datetime
 now_time = datetime.datetime.now()
 
 print("now:{0}".format(datetime.datetime.now()))
+
+# 当前日期
+print("now day: {0}".format(now_time.date()))
+
+# 当前时间
+print("now day: {0}".format(now_time.time()))
+print("now day: {0}".format(datetime.datetime.today()))
+print("now day: {0}".format(now_time.year))
+print("now day: {0}".format(now_time.month))
+print("now day: {0}".format(now_time.day))
