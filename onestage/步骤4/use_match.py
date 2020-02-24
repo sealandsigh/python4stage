0ab12c38eceb624935bd706a8fa23277223fe2ca
sealandsigh <zhujiajunzhu@qq.com>
2020-02-24 22:49:53 +0800

    正则匹配

---
+++ onestage/步骤4/use_match.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2020/2/24
+
+import re
+
+# 将正则表达式编译
+pattern = re.compile(r'hello',re.I)
+print(dir(pattern))
+
+# 通过match来进行匹配
+rest = pattern.match("Hello world!")
+print(rest)
