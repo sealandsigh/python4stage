dddac32b41d2b96f7cc9e12c7ee34faa9f0eb078
sealandsigh <zhujiajunzhu@qq.com>
2020-03-17 22:11:12 +0800

    sql预编译

---
+++ twostage/步骤1mysql/examle_3.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+# __author__="jiajun.zhu"
+# DATE:2020/3/17
+
+import mysql.connector
+
+try:
+    con = mysql.connector.connect(
+        host="localhost",port=3306,
+        user="root",password="zhujiajun",
+        database="demo"
+    )
+    con.start_transaction()
+    cursor = con.cursor()
+    sql = "INSERT INTO t_emp(empno,ename,job,mgr,hiredate,sal,comm,deptno) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+    cursor.execute(sql,(9600,"赵娜","SALESMAN",None,"1985-12-1",2500,None,10))
+    con.commit()
+except Exception as e:
+    if "con" in dir():
+        con.rollback()
+    print(e)
+finally:
+    if "con" in dir():
+        con.close()
--- twostage/步骤1mysql/test.py
+++ twostage/步骤1mysql/test.py
@@ -22,8 +22,11 @@ config = {
 
 con = mysql.connector.connect(**config)
 cursor = con.cursor()
-sql = "SELECT empno,ename,hiredate FROM t_emp;"
-rest = cursor.execute(sql)
-for one in cursor:
-    print(one[0],one[1],one[2])
+# sql = "SELECT empno,ename,hiredate FROM t_emp;"
+sql = "INSERT INTO t_emp(empno,ename,job,mgr,hiredate,sal,comm,deptno) VALUES(%s,%s,%s,%s,%s,%s,%s,%s)"
+# rest = cursor.execute(sql)
+# for one in cursor:
+#     print(one[0],one[1],one[2])
+cursor.execute(sql,(9600,"赵娜","SALESMAN",None,"1985-12-1",2500,None,10))
+con.commit()
 con.close()
