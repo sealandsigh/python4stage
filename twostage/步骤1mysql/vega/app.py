b2856c41abda9dace53015689c0ed5978b56d61e
sealandsigh <zhujiajunzhu@qq.com>
2020-03-24 23:10:25 +0800

    新闻管理完成

--- twostage/步骤1mysql/vega/app.py
+++ twostage/步骤1mysql/vega/app.py
@@ -25,52 +25,88 @@ while True:
     if opt == "1":
         username = input("\n\t用户名:")
         password = getpass("\n\t密码:")
-        result = __user_service.login(username,password)
-        # 登录成功
+        result = __user_service.login(username, password)
+        # 登陆成功
         if result == True:
             # 查询角色
             role = __user_service.search_user_role(username)
-            os.system("clear")
             while True:
+                os.system("cls")
                 if role == "新闻编辑":
-                    print("test")
+                    print('test')
                 elif role == "管理员":
-                    print(Fore.LIGHTGREEN_EX,"\n\t1 新闻管理")
-                    print(Fore.LIGHTGREEN_EX,"\n\t2 用户管理")
-                    print(Fore.LIGHTRED_EX,"\n\tback 退出登录")
-                    print(Fore.LIGHTRED_EX,"\n\texit 退出系统")
+                    print(Fore.LIGHTGREEN_EX, "\n\t1.新闻管理")
+                    print(Fore.LIGHTGREEN_EX, "\n\t2.用户管理")
+                    print(Fore.LIGHTRED_EX, "\n\tback.退出登陆")
+                    print(Fore.LIGHTRED_EX, "\n\texit.退出系统")
                     print(Style.RESET_ALL)
                     opt = input("\n\t输入操作编号:")
                     if opt == "1":
-                        page = 1
                         while True:
-                            os.system("clear")
-                            count_page = __news_service.search_unreview_count_page()
-                            result = __news_service.search_unreview_list(page)
-                            for index in range(len(result)):
-                                one = result[index]
-                                print(Fore.LIGHTBLUE_EX, "\n\t%d\t%s\t%s\t%s" % (index + 1, one[1], one[2], one[3]))
-                            print(Fore.LIGHTBLUE_EX, "\n\t-------------------")
-                            print(Fore.LIGHTBLUE_EX, "\n\t%d/%d" % (page, count_page))
-                            print(Fore.LIGHTBLUE_EX, "\n\t-------------------")
+                            os.system("cls")
+                            print(Fore.LIGHTGREEN_EX, "\n\t1.审批新闻")
+                            print(Fore.LIGHTGREEN_EX, "\n\t2.删除新闻")
                             print(Fore.LIGHTRED_EX, "\n\tback.返回上一层")
-                            print(Fore.LIGHTRED_EX, "\n\tprev.上一页")
-                            print(Fore.LIGHTRED_EX, "\n\tnext.下一页")
                             print(Style.RESET_ALL)
                             opt = input("\n\t输入操作编号:")
-                            if opt == "back":
+                            if opt == "1":
+                                page = 1
+                                while True:
+                                    os.system("cls")
+                                    count_page = __news_service.search_unreview_count_page()
+                                    result = __news_service.search_unreview_list(page)
+                                    for index in range(len(result)):
+                                        one = result[index]
+                                        print(Fore.LIGHTBLUE_EX,
+                                              "\n\t%d\t%s\t%s\t%s" % (index + 1, one[1], one[2], one[3]))
+                                    print(Fore.LIGHTBLUE_EX, "\n\t-------------------")
+                                    print(Fore.LIGHTBLUE_EX, "\n\t%d/%d" % (page, count_page))
+                                    print(Fore.LIGHTBLUE_EX, "\n\t-------------------")
+                                    print(Fore.LIGHTRED_EX, "\n\tback.返回上一层")
+                                    print(Fore.LIGHTRED_EX, "\n\tprev.上一页")
+                                    print(Fore.LIGHTRED_EX, "\n\tnext.下一页")
+                                    print(Style.RESET_ALL)
+                                    opt = input("\n\t输入操作编号:")
+                                    if opt == "back":
+                                        break
+                                    elif opt == "prev" and page > 1:
+                                        page -= 1
+                                    elif opt == "next" and page < count_page:
+                                        page += 1
+                                    elif int(opt) >= 1 and int(opt) <= 10:
+                                        news_id = result[int(opt) - 1][0]
+                                        __news_service.update_unreview_news(news_id)
+                            elif opt == "2":
+                                page = 1
+                                while True:
+                                    os.system("cls")
+                                    count_page = __news_service.search_count_page()
+                                    result = __news_service.search_list(page)
+                                    for index in range(len(result)):
+                                        one = result[index]
+                                        print(Fore.LIGHTBLUE_EX,
+                                              "\n\t%d\t%s\t%s\t%s" % (index + 1, one[1], one[2], one[3]))
+                                    print(Fore.LIGHTBLUE_EX, "\n\t-------------------")
+                                    print(Fore.LIGHTBLUE_EX, "\n\t%d/%d" % (page, count_page))
+                                    print(Fore.LIGHTBLUE_EX, "\n\t-------------------")
+                                    print(Fore.LIGHTRED_EX, "\n\tback.返回上一层")
+                                    print(Fore.LIGHTRED_EX, "\n\tprev.上一页")
+                                    print(Fore.LIGHTRED_EX, "\n\tnext.下一页")
+                                    print(Style.RESET_ALL)
+                                    opt = input("\n\t输入操作编号:")
+                                    if opt == "back":
+                                        break
+                                    elif opt == "prev" and page > 1:
+                                        page -= 1
+                                    elif opt == "next" and page < count_page:
+                                        page += 1
+                                    elif int(opt) >= 1 and int(opt) <= 10:
+                                        news_id = result[int(opt) - 1][0]
+                                        __news_service.delete_by_id(news_id)
+                            elif opt == "back":
                                 break
-                            elif opt == "prev" and page > 1:
-                                page -= 1
-                            elif opt == "next" and page < count_page:
-                                page += 1
-                            elif int(opt) >= 1 and int(opt) <= 10:
-                                news_id = result[int(opt) - 1][0]
-                                __news_service.update_unreview_news(news_id)
-                    if opt == "back":
-                        break
-                    elif opt == "exit":
-                        sys.exit(0)
+                            elif opt == "exit":
+                                sys.exit(0)
         else:
             print("\n\t 登录失败")
             time.sleep(3)
--- twostage/步骤1mysql/vega/db/news_dao.py
+++ twostage/步骤1mysql/vega/db/news_dao.py
@@ -41,6 +41,74 @@ class NewsDao(object):
             if "con" in dir():
                 con.close()
 
+    # 审批新闻
+    def update_unreview_news(self, id):
+        try:
+            con = pool.get_connection()
+            con.start_transaction()
+            cursor = con.cursor()
+            sql = "UPDATE t_news SET state=%s WHERE id=%s"
+            cursor.execute(sql, ("已审批", id))
+            con.commit()
+        except Exception as e:
+            if "con" in dir():
+                con.rollback()
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
+
+    # 查询新闻列表
+    def search_list(self, page):
+        try:
+            con = pool.get_connection()
+            cursor = con.cursor()
+            sql = "SELECT n.id,n.title,t.type,u.username " \
+                  "FROM t_news n JOIN t_type t ON n.type_id=t.id " \
+                  "JOIN t_user u ON n.editor_id=u.id " \
+                  "ORDER BY n.create_time DESC " \
+                  "LIMIT %s,%s"
+            cursor.execute(sql, ((page - 1) * 10, 10))
+            result = cursor.fetchall()
+            return result
+        except Exception as e:
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
+
+    #查询新闻总页数
+    def search_count_page(self):
+        try:
+            con=pool.get_connection()
+            cursor=con.cursor()
+            sql="SELECT CEIL(COUNT(*)/10) FROM t_news"
+            cursor.execute(sql)
+            count_page=cursor.fetchone()[0]
+            return count_page
+        except Exception as e:
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
+
+    #删除新闻
+    def delete_by_id(self,id):
+        try:
+            con = pool.get_connection()
+            con.start_transaction()
+            cursor=con.cursor()
+            sql="DELETE FROM t_news WHERE id=%s"
+            cursor.execute(sql,[id])
+            con.commit()
+        except Exception as e:
+            if "con" in dir():
+                con.rollback()
+            print(e)
+        finally:
+            if "con" in dir():
+                con.close()
+
 if __name__ == "__main__":
     service = NewsDao()
     result = service.search_unreview_list(1)
