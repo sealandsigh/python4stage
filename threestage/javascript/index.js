b17f8315cae376b7bac4fc6dfa36bea4a265508c
sealandsigh <zhujiajunzhu@qq.com>
2020-04-19 21:11:23 +0800

    javascript 内置字符函数

--- threestage/javascript/index.js
+++ threestage/javascript/index.js
@@ -1,3 +1,4 @@
+/*
 var myscore = 100;
 var str;
 var n = parseInt(myscore/10);
@@ -15,3 +16,19 @@ switch (n){
         str = "jige"
 }
 console.log(str)
+*/
+
+// 1+2+3+4+5+6 =5050
+/*
+var i;
+var sum=0;
+for (i=0;i<=100;i++){
+    sum = sum + i;
+}
+console.log(sum)
+/*
+ */
+
+var str = "5201314199006061234"
+console.log(str.substr(0,3))
+console.log(str.substring(0,3))
