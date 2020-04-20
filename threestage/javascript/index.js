a76d3f305863e94d279dc1eec34f8bdec18b1611
sealandsigh <zhujiajunzhu@qq.com>
2020-04-20 18:25:56 +0800

    javascript 表单练习

--- threestage/javascript/index.js
+++ threestage/javascript/index.js
@@ -29,6 +29,67 @@ console.log(sum)
 /*
  */
 
+/*
 var str = "5201314199006061234"
 console.log(str.substr(0,3))
 console.log(str.substring(0,3))
+ */
+
+/*
+var d1 = new Date();
+var d2 = new Date("2020-1-1 10:58:58")
+console.log(d2.getDate())
+*/
+
+/*
+var arrayObj=new Array();
+*/
+
+function show1() {
+    //alert("123456")
+    //document.getElementById("userName").value="999"
+    var xb=document.getElementsByName("xb");
+    var xbText;
+    if (xb[0].checked){
+        xbText=xb[0].value;
+    }else {
+        xbText=xb[1].value;
+    }
+    alert(xbText)
+}
+
+function ymd() {
+    var yyyy=document.getElementById("yyyy");
+    var mm=document.getElementById("mm");
+    var dd=document.getElementById("dd");
+    var date=new Date();
+    var year = parseInt(date.getFullYear());
+    initSelect(yyyy,1999,year);
+    initSelect(mm,1,12);
+    initSelect(dd,1,31);
+    var n=yyyy.length;
+    yyyy.selectedIndex=Math.round(n/2);
+}
+
+function initSelect(obj,start,end) {
+    for (var i=start;i<=end;i++){
+        obj.options.add(new Option(i,i))
+    }
+}
+
+function ceshi() {
+    var mm=document.getElementById("mm");
+    var dd=document.getElementById("dd");
+    var m=parseInt(mm.value);
+
+    var dayEnd;
+    if (m==4 || m==6 || m==9 || m=11){
+        dayEnd=30;
+    }else if{
+        dayEnd=28;
+    }else {
+        dayEnd=31;
+    }
+    dd.options.length=0;
+    initSelect(dd,1,dayEnd);
+}
