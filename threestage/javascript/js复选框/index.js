80b398ca3e7bef1918d77937887347ad6f190b60
sealandsigh <zhujiajunzhu@qq.com>
2020-04-21 18:20:25 +0800

    javascipt 计算器开始

--- threestage/javascript/js复选框/index.js
+++ threestage/javascript/js复选框/index.js
@@ -1,1 +1,19 @@
-
+var flag = true;
+function checkInterest() {
+    var interest = document.getElementsByName("interest");
+    for (i=0;i<interest.length;i++){
+        interest[i].checked=flag;
+    }
+    if (flag){
+        document.getElementById("btn1").value="全不选"
+    }else {
+        document.getElementById("btn1").value="全选"
+    }
+    flag = !flag //开关按钮
+}
+function checkInterest1() {
+    var interest = document.getElementsByName("interest");
+    for (i=0;i<interest.length;i++){
+        interest[i].checked=!interest[i].checked;
+    }
+}
